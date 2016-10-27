5e1e221f59754a8afd66b100a458b5e3f76656bb
Gorka Hernandez <gorka.hernandez@datagenicgroup.com>
2016-10-27 12:14:26 +0200

    Check for IPv6 addresses

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,15 @@
 export class Parser {
   static parseRequest(req) {
     return {
-      ipaddress: req.connection.remoteAddress || "Could not get IP Address",
+      ipaddress: Parser.getIP(req.connection.remoteAddress),
       language: Parser.getLanguage(req.headers["accept-language"]),
       software: Parser.getOS(req.headers["user-agent"])
     }
+  }
+
+  static getIP(remoteAddress) {
+    let isV6 = remoteAddress.indexOf(':') >= 0;
+    return isV6 ? remoteAddress.split(':').reverse()[0] : remoteAddress;
   }
 
   static getOS(userAgent) {
